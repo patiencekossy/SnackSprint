aa8b1b0e00a81baf7ef1ce157ec9eabb1be95acf
mktowett <marvintowett@gmail.com>
2023-05-12 17:27:59 +0300

    completed cart calculations

--- app/src/main/java/com/example/snacksprint/cart/CartActivity.kt
+++ app/src/main/java/com/example/snacksprint/cart/CartActivity.kt
@@ -28,25 +28,54 @@ import cz.msebera.android.httpclient.Header
 import cz.msebera.android.httpclient.entity.StringEntity
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.DecimalFormat
 
 class CartActivity : BaseActivity() {
     lateinit var cartItemsAdapter: CartItemsAdapter
     lateinit var recyclerAdapter: CartAdapter //call the adapter
     lateinit var progressbar: ProgressBar
     lateinit var recyclerView: RecyclerView
+    lateinit var tvTotal: TextView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
         recyclerView = findViewById(R.id.rvCartItems)
+        tvTotal = findViewById(R.id.tvTotal)
 
         cartItemsAdapter = CartItemsAdapter(cartList = getCartItems().toList(), this)
 
         recyclerView.layoutManager = LinearLayoutManager(applicationContext)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = cartItemsAdapter
+        val value = getCartValue(getCartItems().toList())
+        tvTotal.text = "KES $value"
 
+    }
+
+    private fun getCartValue(cartList: List<CartModel>): String{
+        var totalValue = 0.0
+        for (cartModel in cartList) {
+            val itemValue = cartModel.price.toInt() * cartModel.units.toInt()
+            totalValue += itemValue
+        }
+        return formatNumber(totalValue.toInt())
+    }
+
+    private fun formatNumber(price: Int): String{
+        val formPrice = price/10
+        var fmamo = "0.00"
+        try {
+            val amou = formPrice.toDouble()
+            if (amou > 0) {
+                val df = DecimalFormat("#,###.00")
+                fmamo = df.format(amou)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return fmamo
     }
 
 }
